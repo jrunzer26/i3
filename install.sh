b05ed31c2e2857f74c48c11cd49bfc0c7c9d8f23
jrunzer26 <jason.runzer@uoit.net>
2018-04-25 22:04:06 -0400

    added config files and install script

--- install.sh
+++ install.sh
@@ -1,1 +1,5 @@
 echo install script
+ln i3status/config ~/.config/i3status/config
+ln i3config/config ~/.config/i3/config
+ln vimconfig/vimrc ~/.vimrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
