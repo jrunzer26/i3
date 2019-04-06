f204afd644142c84415a8732af37ac936dc7382b
jrunzer26 <jason.runzer@uoit.net>
2019-04-06 18:07:39 -0400

    updated i3status links to remove previous config file

--- i3status-links.sh
+++ i3status-links.sh
@@ -2,6 +2,8 @@ echo "fix config links"
 
 mkdir ~/.config/i3
 mkdir ~/.config/i3status
+rm ~/.config/i3status/config
+rm ~/.config/i3/config
 ln i3status/config ~/.config/i3status/config
 ln i3config/config ~/.config/i3/config
 mkdir ~/Wallpapers
